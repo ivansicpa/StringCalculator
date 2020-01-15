3d542ebdb4390d6e715e8608caafbe2480cc959a
Alvarez, Ivan <Ivan.Alvarez@sicpa.com>
2020-01-15 12:27:21 +0100

    Added Project

---
+++ StringCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StringCalculator
+//
+//  Created by Alvarez, Ivan on 15/01/2020.
+//  Copyright © 2020 Alvarez, Ivan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
