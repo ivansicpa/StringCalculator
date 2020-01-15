ed118dcd1389bc43c03e4d9a41075e7f45a14a11
Alvarez, Ivan <Ivan.Alvarez@sicpa.com>
2020-01-15 12:48:09 +0100

    -

---
+++ StringCalculator/StringCalculator.swift
@@ -0,0 +1,14 @@
+//
+//  StringCalculator.swift
+//  StringCalculator
+//
+//  Created by Alvarez, Ivan on 15/01/2020.
+//  Copyright © 2020 Alvarez, Ivan. All rights reserved.
+//
+
+import Foundation
+
+
+class StringCalculator {
+    
+}
